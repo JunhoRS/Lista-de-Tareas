d3594e2901cfb534e726bf88da948c9ebc49800f
JunhoRS <126283654+JunhoRS@users.noreply.github.com>
2023-07-03 18:05:08 -0400

    added project lista de tareas

---
+++ Lista de Tareas/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace Lista_de_Tareas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.txtTitulo = new System.Windows.Forms.TextBox();
+            this.btnMarcarCompletada = new System.Windows.Forms.Button();
+            this.btnEliminarTarea = new System.Windows.Forms.Button();
+            this.lstTareasCompletadas = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chkCompletada = new System.Windows.Forms.CheckBox();
+            this.dataGridViewTareas = new System.Windows.Forms.DataGridView();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.colorDialog2 = new System.Windows.Forms.ColorDialog();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.colorDialog3 = new System.Windows.Forms.ColorDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTareas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(134, 47);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(117, 20);
+            this.txtDescripcion.TabIndex = 0;
+            // 
+            // txtTitulo
+            // 
+            this.txtTitulo.Location = new System.Drawing.Point(134, 19);
+            this.txtTitulo.Name = "txtTitulo";
+            this.txtTitulo.Size = new System.Drawing.Size(117, 20);
+            this.txtTitulo.TabIndex = 1;
+            // 
+            // btnMarcarCompletada
+            // 
+            this.btnMarcarCompletada.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.btnMarcarCompletada.Location = new System.Drawing.Point(443, 374);
+            this.btnMarcarCompletada.Name = "btnMarcarCompletada";
+            this.btnMarcarCompletada.Size = new System.Drawing.Size(75, 23);
+            this.btnMarcarCompletada.TabIndex = 3;
+            this.btnMarcarCompletada.Text = "Completada";
+            this.btnMarcarCompletada.UseVisualStyleBackColor = true;
+            this.btnMarcarCompletada.Click += new System.EventHandler(this.btnMarcarCompletada_Click);
+            // 
+            // btnEliminarTarea
+            // 
+            this.btnEliminarTarea.Location = new System.Drawing.Point(538, 374);
+            this.btnEliminarTarea.Name = "btnEliminarTarea";
+            this.btnEliminarTarea.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminarTarea.TabIndex = 4;
+            this.btnEliminarTarea.Text = "Borrar";
+            this.btnEliminarTarea.UseVisualStyleBackColor = true;
+            this.btnEliminarTarea.Click += new System.EventHandler(this.btnEliminarTarea_Click);
+            // 
+            // lstTareasCompletadas
+            // 
+            this.lstTareasCompletadas.FormattingEnabled = true;
+            this.lstTareasCompletadas.Location = new System.Drawing.Point(12, 249);
+            this.lstTareasCompletadas.Name = "lstTareasCompletadas";
+            this.lstTareasCompletadas.Size = new System.Drawing.Size(239, 160);
+            this.lstTareasCompletadas.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(176, 73);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Agregar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Tarea por añadir: ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Descripción";
+            // 
+            // chkCompletada
+            // 
+            this.chkCompletada.AutoSize = true;
+            this.chkCompletada.Location = new System.Drawing.Point(27, 79);
+            this.chkCompletada.Name = "chkCompletada";
+            this.chkCompletada.Size = new System.Drawing.Size(88, 17);
+            this.chkCompletada.TabIndex = 9;
+            this.chkCompletada.Text = "Completada?";
+            this.chkCompletada.UseVisualStyleBackColor = true;
+            // 
+            // dataGridViewTareas
+            // 
+            this.dataGridViewTareas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewTareas.Location = new System.Drawing.Point(284, 12);
+            this.dataGridViewTareas.Name = "dataGridViewTareas";
+            this.dataGridViewTareas.Size = new System.Drawing.Size(476, 356);
+            this.dataGridViewTareas.TabIndex = 10;
+            this.dataGridViewTareas.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewTareas_CellContentClick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 224);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(151, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "TAREAS COMPLETADAS";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(773, 504);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dataGridViewTareas);
+            this.Controls.Add(this.chkCompletada);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lstTareasCompletadas);
+            this.Controls.Add(this.btnEliminarTarea);
+            this.Controls.Add(this.btnMarcarCompletada);
+            this.Controls.Add(this.txtTitulo);
+            this.Controls.Add(this.txtDescripcion);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTareas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.TextBox txtTitulo;
+        private System.Windows.Forms.Button btnMarcarCompletada;
+        private System.Windows.Forms.Button btnEliminarTarea;
+        private System.Windows.Forms.ListBox lstTareasCompletadas;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox chkCompletada;
+        private System.Windows.Forms.DataGridView dataGridViewTareas;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.ColorDialog colorDialog2;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.ColorDialog colorDialog3;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Lista de Tareas/Form1.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Lista_de_Tareas
+{
+    public partial class Form1 : Form
+    {
+        // Add these lines inside the Form1 class, after InitializeComponent();
+        private ComboBox cmbBackgroundColor;
+        private ComboBox cmbFontSize;
+        private Button btnApplyConfig;
+
+
+        public Form1()
+        {
+
+            InitializeComponent();
+
+        }
+        private void BtnApplyConfig_Click(object sender, EventArgs e)
+        {
+            // Get the selected background color and font size from the ComboBoxes
+            string selectedColor = cmbBackgroundColor.SelectedItem.ToString();
+            int selectedFontSize = int.Parse(cmbFontSize.SelectedItem.ToString());
+
+            // Apply the selected background color to the DataGridView
+            dataGridViewTareas.BackgroundColor = Color.FromName(selectedColor);
+
+            // Apply the selected font size to the DataGridView's font
+            dataGridViewTareas.Font = new Font(dataGridViewTareas.Font.FontFamily, selectedFontSize);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            cmbBackgroundColor = new ComboBox();
+            cmbBackgroundColor.Items.AddRange(new string[] { "White", "LightGray", "LightBlue", "Yellow" }); // Add color options
+            cmbBackgroundColor.SelectedIndex = 0; // Set default background color
+
+            cmbFontSize = new ComboBox();
+            cmbFontSize.Items.AddRange(new string[] { "8", "10", "12", "14", "16", "18", "20" }); // Add font size options
+            cmbFontSize.SelectedIndex = 2; // Set default font size
+
+            btnApplyConfig = new Button();
+            btnApplyConfig.Text = "Apply Configurations";
+            btnApplyConfig.Click += BtnApplyConfig_Click; // Event handler for apply configurations button
+
+            // Add the controls to your form's controls collection and set their positions
+            // You can set the positions as per your form layout.
+            cmbBackgroundColor.Location = new Point(10, 450);
+            cmbFontSize.Location = new Point(150, 450);
+            btnApplyConfig.Location = new Point(300, 450);
+
+            this.Controls.Add(cmbBackgroundColor);
+            this.Controls.Add(cmbFontSize);
+            this.Controls.Add(btnApplyConfig);
+
+            dataGridViewTareas.Columns.Add("Id", "ID");
+            dataGridViewTareas.Columns.Add("Titulo", "Título");
+            dataGridViewTareas.Columns.Add("Descripcion", "Descripción");
+            dataGridViewTareas.Columns.Add("Completada", "Completada");
+
+
+            CargarTareas();
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+              GuardarTareas();
+        }
+        private void btnAgregarTarea_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection conexion = new SqlConnection("Server=DESKTOP-7FTGLP1; database=dblistatareas; Integrated security = true");
+            conexion.Open();
+            string titulo = txtTitulo.Text;
+            string descripcion = txtDescripcion.Text;
+            bool completada = chkCompletada.Checked ? true : false;
+            string cadena = "INSERT INTO Tareas (Titulo, Descripcion, Completada) VALUES (@Titulo, @Descripcion, @Completada)";
+            SqlCommand comando = new SqlCommand(cadena, conexion);
+            comando.Parameters.AddWithValue("@Titulo", titulo);
+            comando.Parameters.AddWithValue("@Descripcion", descripcion);
+            comando.Parameters.AddWithValue("@Completada", completada);
+
+            comando.ExecuteNonQuery();
+            MessageBox.Show("Los datos se ingresaron de forma satisfactoria");
+
+
+            conexion.Close();
+            CargarTareas();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void CargarTareas()
+        {
+            try
+            {
+                dataGridViewTareas.Rows.Clear();
+                lstTareasCompletadas.Items.Clear();
+
+                using (SqlConnection conexion = new SqlConnection("Server=DESKTOP-7FTGLP1; database=dblistatareas; Integrated security=true"))
+                {
+                    conexion.Open();
+                    string consulta = "SELECT Id, Titulo, Descripcion, Completada FROM Tareas";
+                    SqlDataAdapter adaptador = new SqlDataAdapter(consulta, conexion);
+                    DataTable tablaTareas = new DataTable();
+                    adaptador.Fill(tablaTareas);
+
+                    foreach (DataRow fila in tablaTareas.Rows)
+                    {
+                        int id = Convert.ToInt32(fila["Id"]);
+                        string titulo = fila["Titulo"] != DBNull.Value ? fila["Titulo"].ToString() : string.Empty;
+                        string descripcion = fila["Descripcion"] != DBNull.Value ? fila["Descripcion"].ToString() : string.Empty;
+                        bool completada = fila["Completada"] != DBNull.Value && Convert.ToBoolean(fila["Completada"]);
+
+                        int rowIndex = dataGridViewTareas.Rows.Add(id, titulo, descripcion, completada);
+
+                        if (completada)
+                        {
+                            dataGridViewTareas.Rows[rowIndex].DefaultCellStyle.BackColor = Color.LightGreen;
+                            lstTareasCompletadas.Items.Add(titulo);
+                        }
+                    }
+
+                    conexion.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar las tareas: " + ex.Message);
+            }
+        }
+
+
+        private void dataGridViewTareas_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            {
+                if (e.ColumnIndex == dataGridViewTareas.Columns["Completada"].Index && e.RowIndex >= 0)
+                {
+
+                    bool completada = (bool)dataGridViewTareas.Rows[e.RowIndex].Cells["Completada"].Value;
+
+                    completada = !completada;
+
+                    // Obtener el Id y el título de la tarea de la fila seleccionada
+                    int idTarea = (int)dataGridViewTareas.Rows[e.RowIndex].Cells["Id"].Value;
+                    string tituloTarea = dataGridViewTareas.Rows[e.RowIndex].Cells["Titulo"].Value.ToString();
+                    using (SqlConnection conexion = new SqlConnection("Server=DESKTOP-7FTGLP1; database=dblistatareas; Integrated security = true"))
+                    {
+                        string consulta = "UPDATE Tareas SET Completada = @Completada WHERE Id = @Id";
+                        SqlCommand comando = new SqlCommand(consulta, conexion);
+                        comando.Parameters.AddWithValue("@Completada", completada);
+                        comando.Parameters.AddWithValue("@Id", idTarea);
+
+                        try
+                        {
+                            conexion.Open();
+                            int filasActualizadas = comando.ExecuteNonQuery();
+                            if (filasActualizadas > 0)
+                            {
+                                if (completada)
+                                {
+                                    MessageBox.Show("Tarea marcada como completada correctamente.");
+                                    // Mueve el título de la tarea completada al ListBox
+                                    lstTareasCompletadas.Items.Add(tituloTarea);
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Tarea marcada como pendiente correctamente.");
+                                    lstTareasCompletadas.Items.Remove(tituloTarea);
+                                    CargarTareas();
+                                }
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Error al marcar la tarea como completada: " + ex.Message);
+                        }
+                    }
+
+                }
+            }
+
+        }
+
+        private void btnEliminarTarea_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewTareas.SelectedRows.Count > 0)
+            {
+                int idTarea = Convert.ToInt32(dataGridViewTareas.SelectedRows[0].Cells["Id"].Value);
+
+                DialogResult resultado = MessageBox.Show("¿Estás seguro de que deseas eliminar esta tarea?", "Confirmar Eliminación", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (resultado == DialogResult.Yes)
+                {
+                    SqlConnection conexion = new SqlConnection("Server=DESKTOP-7FTGLP1; database=dblistatareas; Integrated security=true");
+                    string consulta = "DELETE FROM Tareas WHERE Id = @Id";
+                    SqlCommand comando = new SqlCommand(consulta, conexion);
+                    comando.Parameters.AddWithValue("@Id", idTarea);
+
+                    try
+                    {
+                        conexion.Open();
+                        int filasEliminadas = comando.ExecuteNonQuery();
+                        if (filasEliminadas > 0)
+                        {
+                            MessageBox.Show("Tarea eliminada correctamente.");
+                            CargarTareas();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error al eliminar la tarea: " + ex.Message);
+                    }
+                    finally
+                    {
+                        conexion.Close();
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor, seleccione una tarea para eliminar.", "Tarea no seleccionada", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnMarcarCompletada_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewTareas.SelectedRows.Count > 0)
+            {
+                int idTarea = Convert.ToInt32(dataGridViewTareas.SelectedRows[0].Cells["Id"].Value);
+
+                using (SqlConnection conexion = new SqlConnection("Server=DESKTOP-7FTGLP1; database=dblistatareas; Integrated security = true"))
+                {
+                    string consulta = "UPDATE Tareas SET Completada = 1 WHERE Id = @Id";
+                    SqlCommand comando = new SqlCommand(consulta, conexion);
+                    comando.Parameters.AddWithValue("@Id", idTarea);
+
+                    try
+                    {
+                        conexion.Open();
+                        int filasActualizadas = comando.ExecuteNonQuery();
+                        if (filasActualizadas > 0)
+                        {
+                            MessageBox.Show("Tarea marcada como completada correctamente.");
+                            CargarTareas();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error al marcar la tarea como completada: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void GuardarTareas()
+        {
+            try
+            {
+                using (SqlConnection conexion = new SqlConnection("Server=DESKTOP-7FTGLP1; database=dblistatareas; Integrated security=true"))
+                {
+                    conexion.Open();
+                    foreach (DataGridViewRow row in dataGridViewTareas.Rows)
+                    {
+                        int id = Convert.ToInt32(row.Cells["Id"].Value);
+                        string titulo = row.Cells["Titulo"].Value.ToString();
+                        string descripcion = row.Cells["Descripcion"].Value.ToString();
+                        bool completada = Convert.ToBoolean(row.Cells["Completada"].Value);
+
+                        string existeConsulta = "SELECT COUNT(*) FROM Tareas WHERE Id = @Id";
+                        SqlCommand existeComando = new SqlCommand(existeConsulta, conexion);
+                        existeComando.Parameters.AddWithValue("@Id", id);
+                        int existe = Convert.ToInt32(existeComando.ExecuteScalar());
+
+                        if (existe > 0)
+                        {
+                            // If the task exists, update it in the database
+                            string actualizarConsulta = "UPDATE Tareas SET Titulo = @Titulo, Descripcion = @Descripcion, Completada = @Completada WHERE Id = @Id";
+                            SqlCommand actualizarComando = new SqlCommand(actualizarConsulta, conexion);
+                            actualizarComando.Parameters.AddWithValue("@Id", id);
+                            actualizarComando.Parameters.AddWithValue("@Titulo", titulo);
+                            actualizarComando.Parameters.AddWithValue("@Descripcion", descripcion);
+                            actualizarComando.Parameters.AddWithValue("@Completada", completada);
+                            actualizarComando.ExecuteNonQuery();
+                        }
+                        else
+                        {
+                            string insertarConsulta = "INSERT INTO Tareas (Id, Titulo, Descripcion, Completada) VALUES (@Id, @Titulo, @Descripcion, @Completada)";
+                            SqlCommand insertarComando = new SqlCommand(insertarConsulta, conexion);
+                            insertarComando.Parameters.AddWithValue("@Id", id);
+                            insertarComando.Parameters.AddWithValue("@Titulo", titulo);
+                            insertarComando.Parameters.AddWithValue("@Descripcion", descripcion);
+                            insertarComando.Parameters.AddWithValue("@Completada", completada);
+                            insertarComando.ExecuteNonQuery();
+                        }
+                    }
+                    conexion.Close();
+                }
+                MessageBox.Show("Tareas guardadas correctamente.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al guardar las tareas: " + ex.Message);
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
